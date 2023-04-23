078fe2fe1d0c9711dad2e100bbd59afbb4357134
shshshshsjsj <101315628+shshshshsjsj@users.noreply.github.com>
2023-04-23 10:33:31 +0800

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>Openlayers 基础示例</h2>
+    <p>主要技术：Openlayers6 + Vue2 + AntdV</p>
+    <p>内容：Openlayers6 部分基础示例</p>
+    <p>
+      挑选项目中的典型示例做成 Example，方便参考，目前只有部分示例，后期会逐渐增加
+    </p>
+    <p>若有比较通用的示例可以提 Issues 或 PR</p>
+    <br />
+    <br />
+    <h4>Overlay</h4>
+    <p>
+      Overlay
+      叠加图层：主要是放置一些和地图位置相关的元素，如：infowindow、点标记、图片等，而这些覆盖物都是和html中的element等价的，通过overlay的属性element和html元素绑定同时设定坐标参数——达到将html元素放到地图上的位置，在平移缩放的时候html元素也会随着地图的移动而移动。
+    </p>
+    <br />
+    <h4>VectorLayer</h4>
+    <p>
+      VectorLayer矢量图层： 矢量图层是在客户端渲染的，在 web
+      环境中，对应的就是浏览器。构成一个矢量图层的包含一个数据（source）和一个样式（style），数据构成矢量图层的要素，样式规定要素显示的方式和外观。一个初始化成功的矢量图层包含一个到多个要素（feature），每个要素由地理属（geometry）和多个其他的属性，可能包含名称等。
+    </p>
+    <br />
+    <h3>TODO</h3>
+    <h4><b>=== 交互 ===</b></h4>
+    <p class="delete">框选 Overlay</p>
+    <p>框选 Feature</p>
+    <h4><b>=== 动画 ===</b></h4>
+    <p class="delete">设备报警</p>
+    <p class="delete">防区报警</p>
+    <p>光纤报警</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style lang="scss" scoped>
+.delete{
+  text-decoration: line-through;
+}
+p{
+  text-indent: 2rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,319 @@
+import VueRouter from 'vue-router'
+import Home from '@/pages/Home.vue'
+
+const router = new VueRouter({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      label: '首页',
+      root: null,
+      current: 'Home'
+    }
+  }, {
+    path: '/baseMap',
+    name: 'BaseMap',
+    component: () => import('@/pages/BaseMap.vue'),
+    meta: {
+      label: '基础地图',
+      root: null,
+      current: 'BaseMap'
+    }
+  }, {
+    path: '/overlay/',
+    name: 'Overlay',
+    component: () => import('@/pages/Overlay/index.vue'),
+    meta: {
+      label: 'Overlay 叠加层',
+      root: null,
+      current: 'Overlay'
+    },
+    children: [{
+      path: 'marker',
+      name: 'Marker',
+      component: () => import('@/pages/Overlay/Marker.vue'),
+      meta: {
+        label: '标记点',
+        root: 'Overlay',
+        current: 'Marker'
+      }
+    }]
+  }, {
+    path: '/vectorLayer/',
+    name: 'VectorLayer',
+    component: () => import('@/pages/VectorLayer/index.vue'),
+    meta: {
+      label: 'VectorLayer 矢量图层',
+      root: null,
+      current: 'VectorLayer'
+    },
+    children: [{
+      path: 'point',
+      name: 'Point',
+      component: () => import('@/pages/VectorLayer/Point.vue'),
+      meta: {
+        label: 'Point 点',
+        root: 'VectorLayer',
+        current: 'Point'
+      }
+    }, {
+      path: 'lineString',
+      name: 'LineString',
+      component: () => import('@/pages/VectorLayer/LineString.vue'),
+      meta: {
+        label: 'LineString 线',
+        root: 'VectorLayer',
+        current: 'LineString'
+      }
+    }, {
+      path: 'polygon',
+      name: 'Polygon',
+      component: () => import('@/pages/VectorLayer/Polygon.vue'),
+      meta: {
+        label: 'Polygon 多边形(面)',
+        root: 'VectorLayer',
+        current: 'Polygon'
+      }
+    }, {
+      path: 'multiPoint',
+      name: 'MultiPoint',
+      component: () => import('@/pages/VectorLayer/MultiPoint.vue'),
+      meta: {
+        label: 'MultiPoint 多点',
+        root: 'VectorLayer',
+        current: 'MultiPoint'
+      }
+    }, {
+      path: 'multiLineString',
+      name: 'MultiLineString',
+      component: () => import('@/pages/VectorLayer/MultiLineString.vue'),
+      meta: {
+        label: 'MultiLineString 多线',
+        root: 'VectorLayer',
+        current: 'MultiLineString'
+      }
+    }, {
+      path: 'multiPolygon',
+      name: 'MultiPolygon',
+      component: () => import('@/pages/VectorLayer/MultiPolygon.vue'),
+      meta: {
+        label: 'MultiPolygon 多面',
+        root: 'VectorLayer',
+        current: 'MultiPolygon'
+      }
+    }, {
+      path: 'arrow',
+      name: 'Arrow',
+      component: () => import('@/pages/VectorLayer/Arrow.vue'),
+      meta: {
+        label: '箭头',
+        root: 'VectorLayer',
+        current: 'Arrow'
+      }
+    }, {
+      path: 'sector',
+      name: 'Sector',
+      component: () => import('@/pages/VectorLayer/Sector.vue'),
+      meta: {
+        label: '扇形',
+        root: 'VectorLayer',
+        current: 'Sector'
+      }
+    }, {
+      path: 'radar',
+      name: 'Radar',
+      component: () => import('@/pages/VectorLayer/Radar.vue'),
+      meta: {
+        label: '雷达',
+        root: 'VectorLayer',
+        current: 'Radar'
+      }
+    },]
+  },
+  {
+    path: '/animation/',
+    name: 'Animation',
+    component: () => import('@/pages/Animation/index.vue'),
+    meta: {
+      label: 'Animation 动画'
+    },
+    children: [{
+      path: 'twinklePoint',
+      name: 'TwinklePoint',
+      component: () => import('@/pages/Animation/TwinklePoint.vue'),
+      meta: {
+        label: '闪烁点',
+        root: 'Animation',
+        current: 'TwinklePoint'
+      }
+    }, {
+      path: 'trackLine',
+      name: 'TrackLine',
+      component: () => import('@/pages/Animation/TrackLine.vue'),
+      meta: {
+        label: '轨迹线',
+        root: 'Animation',
+        current: 'TrackLine'
+      }
+    }, {
+      path: 'trackPlayback',
+      name: 'TrackPlayback',
+      component: () => import('@/pages/Animation/TrackPlayback.vue'),
+      meta: {
+        label: '轨迹回放',
+        root: 'Animation',
+        current: 'TrackPlayback'
+      }
+    }, {
+      path: 'radarAnimation',
+      name: 'RadarAnimation',
+      component: () => import('@/pages/Animation/RadarAnimation.vue'),
+      meta: {
+        label: '雷达波',
+        root: 'Animation',
+        current: 'RadarAnimation'
+      }
+    }, {
+      path: 'resPointAlarm',
+      name: 'ResPointAlarm',
+      component: () => import('@/pages/Animation/ResPointAlarm.vue'),
+      meta: {
+        label: '资源点报警',
+        root: 'Animation',
+        current: 'ResPointAlarm'
+      }
+    },{
+      path: 'defenceAlarm',
+      name: 'DefenceAlarm',
+      component: () => import('@/pages/Animation/DefenceAlarm.vue'),
+      meta: {
+        label: '防区报警',
+        root: 'Animation',
+        current: 'DefenceAlarm'
+      }
+    },]
+  },
+  {
+    path: '/interaction/',
+    name: 'Interaction',
+    component: () => import('@/pages/Interaction/index.vue'),
+    meta: {
+      label: 'Interaction 交互'
+    },
+    children: [{
+      path: 'popover',
+      name: 'Popover',
+      component: () => import('@/pages/Interaction/Popover.vue'),
+      meta: {
+        label: 'Popover 点击弹窗',
+        root: 'Interaction',
+        current: 'Popover'
+      }
+    }, {
+      path: 'setCenter',
+      name: 'SetCenter',
+      component: () => import('@/pages/Interaction/SetCenter.vue'),
+      meta: {
+        label: 'SetCenter 设置中心',
+        root: 'Interaction',
+        current: 'SetCenter'
+      }
+    }, {
+      path: 'hoverAndClick',
+      name: 'HoverAndClick',
+      component: () => import('@/pages/Interaction/HoverAndClick.vue'),
+      meta: {
+        label: '矢量图层 Hover和Click',
+        root: 'Interaction',
+        current: 'HoverAndClick'
+      }
+    }, {
+      path: 'switchTileLayer',
+      name: 'SwitchTileLayer',
+      component: () => import('@/pages/Interaction/SwitchTileLayer.vue'),
+      meta: {
+        label: '切换瓦片图层',
+        root: 'Interaction',
+        current: 'SwitchTileLayer'
+      }
+    }, {
+      path: 'showTrackInfo',
+      name: 'ShowTrackInfo',
+      component: () => import('@/pages/Interaction/ShowTrackInfo.vue'),
+      meta: {
+        label: '点击轨迹显示信息',
+        root: 'Interaction',
+        current: 'ShowTrackInfo'
+      }
+    }, {
+      path: 'vectorLayerVisible',
+      name: 'VectorLayerVisible',
+      component: () => import('@/pages/Interaction/VectorLayerVisible.vue'),
+      meta: {
+        label: '矢量图层显示隐藏',
+        root: 'Interaction',
+        current: 'VectorLayerVisible'
+      }
+    }, {
+      path: 'addMarker',
+      name: 'AddMarker',
+      component: () => import('@/pages/Interaction/AddMarker.vue'),
+      meta: {
+        label: '添加标记点',
+        root: 'Interaction',
+        current: 'AddMarker'
+      }
+    }, {
+      path: 'measureDistance',
+      name: 'MeasureDistance',
+      component: () => import('@/pages/Interaction/MeasureDistance.vue'),
+      meta: {
+        label: '测距',
+        root: 'Interaction',
+        current: 'MeasureDistance'
+      }
+    }, {
+      path: 'measureArea',
+      name: 'MeasureArea',
+      component: () => import('@/pages/Interaction/MeasureArea.vue'),
+      meta: {
+        label: '测面',
+        root: 'Interaction',
+        current: 'MeasureArea'
+      }
+    }, {
+      path: 'drawBoxOverlay',
+      name: 'DrawBoxOverlay',
+      component: () => import('@/pages/Interaction/DrawBoxOverlay.vue'),
+      meta: {
+        label: '框选 Overlay',
+        root: 'Interaction',
+        current: 'DrawBoxOverlay'
+      }
+    },]
+  }, {
+    path: '/webGL/',
+    name: 'WebGL',
+    component: () => import('@/pages/WebGL/index.vue'),
+    meta: {
+      label: 'WebGL',
+      root: null,
+      current: 'WebGL'
+    },
+    children: [{
+      path: 'iconPoints',
+      name: 'IconPoints',
+      component: () => import('@/pages/WebGL/IconPoints.vue'),
+      meta: {
+        label: '海量图标',
+        root: 'WebGL',
+        current: 'IconPoints'
+      }
+    }]
+  },
+
+  ]
+})
+
+export default router
